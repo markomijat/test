9c3092e6f7112e763f447b792c5bd6494f51f5ee
mmarko <marrkonni@yahoo.com>
2019-03-30 14:13:25 +0100

    first commit

---
+++ glavni.py
@@ -0,0 +1,26 @@
+import xlwings as xw
+
+# from mymodule import myfunctions as mf
+
+from mymodule import myfunctions as mf
+
+def probaNEW():		
+	"""Retourne Bonjour nom """
+	#~app1 = xw.App()	
+	return mf.proba();
+	# return 'Bonjour svi !'
+
+@xw.func
+@xw.arg('nom', doc="Nom de la personne")
+def hello1(nom):		
+	"""Retourne Bonjour nom """
+	#~app1 = xw.App()
+	m = 1
+	#return 'Bonjour {1}  {0}'.format(nom,m)
+	return mf.proba()
+
+
+# print(probag())
+# print(mf.proba())
+
+print(probaNEW())
